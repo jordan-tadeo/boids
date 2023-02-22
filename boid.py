2123ca2c73fdacd161fd6029f5c55368b487a210
Jordan Tadeo <jtadeo128@gmail.com>
2023-02-22 02:22:01 -0800

    Made changes to boid shape and velocity

--- boid.py
+++ boid.py
@@ -3,12 +3,12 @@ import math
 
 W, H = (640, 480)
 
-boid_size = (32, 32)
+boid_size = (8, 12)
 
 class Boid:
     def __init__(self):
         self.pos = [W/2, H/2]
-        self.vel = [0, 0]
+        self.vel = [1, 0]
         self.hdg = 0
 
         self.surf = pygame.Surface((boid_size[0]*math.sqrt(2), boid_size[1]*math.sqrt(2)))
